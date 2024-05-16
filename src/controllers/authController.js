3c5aeac8e59bf076c05c480375f39a2f5c626698
sakayat <sakhyatkabir1@gmail.com>
2024-05-16 12:58:58 -0500

    added users schema validation

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,50 +1,65 @@
 import { prismaClient } from "../index.js";
 import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
+import { signupSchema } from "../schema/users.js";
 
 const { hashSync, compareSync } = bcrypt;
 
 export const register = async (req, res) => {
+  try {
+    
+    const body = signupSchema.safeParse(req.body)
 
-  const { name, email, password } = req.body;
+    if (!body.success) {
+       return res.status(400).json({ message: body.error.issues[0].message })
+      }
+
+    const { name, email, password } = req.body;
+
+
+
+    let user = await prismaClient.user.findFirst({
+      where: { email: email },
+    });
+
+    if (user) {
+      throw Error("User already exists");
+    }
+
+    user = await prismaClient.user.create({
+      data: {
+        name,
+        email,
+        password: hashSync(password, 10),
+      },
+    });
+    res.json(user);
+
+  } catch (error) {
+    return res.status(404).json({ message: error.message });
+  }
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
 
   let user = await prismaClient.user.findFirst({
     where: { email: email },
   });
 
-  if (user) {
-    throw Error("User already exists");
+  if (!user) {
+    throw Error("User does not exists");
   }
 
-  user = await prismaClient.user.create({
-    data: {
-      name,
-      email,
-      password: hashSync(password, 10),
-    },
+  const isMatch = compareSync(password, user.password);
+
+  if (!isMatch) {
+    return res.json({ message: "Invalid credentials" });
+  }
+
+  const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY, {
+    expiresIn: "1h",
   });
-  res.json(user);
+
+  res.json({ user, token });
 };
-
-export const login = async (req, res) => {
-
-    const { email, password } = req.body;
-  
-    let user = await prismaClient.user.findFirst({
-      where: { email: email },
-    });
-  
-    if (!user) {
-      throw Error("User does not exists");
-    }
-  
-    const isMatch = compareSync(password, user.password)
-
-    if(!isMatch){
-        return res.json({message: "Invalid credentials"})
-    }
-
-    const token = jwt.sign({id: user.id}, process.env.SECRET_KEY, {expiresIn: '1h'})
-
-    res.json({user, token});
-  };
---
+++ src/schema/users.js
@@ -0,0 +1,7 @@
+import { z } from "zod";
+
+export const signupSchema = z.object({
+  name: z.string().min(3, "Must be 3 or more characters long"),
+  email: z.string().email({message: "Invalid email address"}),
+  password: z.string().min(6, "Password is too short"),
+});
